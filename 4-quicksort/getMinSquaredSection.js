dcf7122c605967b2c8610afb085b775cd2d915fd
Andrew Shtyka <andrii.shtyka@gmail.com>
2026-08-12 07:26:24 +0300

    Update getMinSquaredSection.js

--- 4-quicksort/getMinSquaredSection.js
+++ 4-quicksort/getMinSquaredSection.js
@@ -8,6 +8,9 @@ function getMinSquaredSection(width = 0, height = 0) {
     // base case
     if (width === height) return width;
 
+    // avoid stack overflow
+    if (width < 0.5 && height < 0.5) return Number(width.toFixed(2));
+
     // decrease problem
     const minSide = width < height ? width : height;
     const maxSide = width > height ? width : height;
